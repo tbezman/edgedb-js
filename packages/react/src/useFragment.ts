54987dfb99ff0273e2dbe12ca21a489ad83279a1
Terence Bezman <terencebezman@gmail.com>
2023-12-23 20:13:08 -0500

    make useFragment typesafe with dumb workaround

--- packages/react/src/useFragment.ts
+++ packages/react/src/useFragment.ts
@@ -10,39 +10,31 @@ import type { FragmentReturnType } from "../../generate/src/syntax/select";
 
 const clone = rfdc();
 
-export function useFragment<F extends FragmentReturnType<any, any>>(
-  ref: Parameters<F["pull"]>[0],
-  fragment: F
-): ReturnType<F["pull"]> {
+export function useFragment<
+  F extends { type_: string; pull: (ref: any) => any; shape: () => any }
+>(ref: Parameters<F["pull"]>[0], fragment: F): ReturnType<F["pull"]> {
   const context = useContext(EdgeDBContext);
   const setCache = context?.setCache;
   const data = fragment.pull(ref);
-
   if (!context) {
     throw new Error(`useFragment must be used within an EdgeDBProvider`);
   }
-
   const type = findType(context.spec, fragment.type_);
-
   if (!type) {
     throw new Error(`Could not find type ${fragment.type_}`);
   }
-
   useEffect(() => {
     setCache?.((previous) => {
       const cache = clone(previous);
-
       updateCache({
         spec: context?.spec,
         cache,
         data,
         type,
       });
-
       return cache;
     });
   }, []);
-
   const resultFromCache = readFromCache({
     type,
     spec: context.spec,
@@ -51,6 +43,5 @@ export function useFragment<F extends FragmentReturnType<any, any>>(
     shape: fragment.shape()({}),
     id: data.id,
   }) as ReturnType<F["pull"]>;
-
   return resultFromCache ?? data;
 }
