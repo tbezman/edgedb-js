fd2bdbb111a336d05cb73b8c9664f50e295ce8fe
Terence Bezman <terencebezman@gmail.com>
2024-03-09 16:03:51 -0500

    update spinner color

--- packages/nextjs-demo/components/Spinner.tsx
+++ packages/nextjs-demo/components/Spinner.tsx
@@ -2,7 +2,7 @@ export function Spinner({ className }: { className?: string }) {
   return (
     <svg
       aria-hidden="true"
-      className={`text-cyan-200 animate-spin fill-white ${className}`}
+      className={`text-cyan-400 animate-spin fill-white ${className}`}
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
